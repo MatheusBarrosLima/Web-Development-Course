05da277380ff5f321b109187f7c98335c8c9193c
Matheus Barros Lima <matheusmatheusbarroslima6@gmail.com>
2023-11-27 15:59:36 -0300

    add class-02-operators

---
+++ 3-JavaScript/class-02-operators/1-arithmetic.js
@@ -0,0 +1,40 @@
+let num1 = 81;
+let num2 = 3;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+console.log(num1 ** num2);
+console.log(num1 ** (1 / 2));
+console.log(Math.sqrt(num1));
+
+console.log(num2);
+console.log(num2++); 
+console.log(num2++);
+console.log(num2);
+console.log(++num2);
+
+let num = 2;
+    console.log(num);
+    console.log(num--);
+    console.log(num);
+    console.log(--num);
+    console.log(num);
+    console.log(++num + 2);
+
+    // assigment 
+    let number
+
+    number = 30;
+    // number = number + 20;
+
+    number += 2 
+    number -= 2 
+    number *= 2 
+    number /= 2 
+    number %= 2
+    number **= 2
+
+    console.log(number);
