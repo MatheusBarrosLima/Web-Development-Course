a545026170b19ca82da9ae3fccc20d712c63a048
Matheus Barros Lima <matheusmatheusbarroslima6@gmail.com>
2023-11-28 16:22:55 -0300

    add 3-logical

---
+++ 3-JavaScript/class-02-operators/4-types.js
@@ -0,0 +1,23 @@
+//Binary
+let num1 = 3
+let num2 = 2
+
+console.log(num1 / num2);
+
+//Unary
+
+console.log(++num1);
+
+console.log(typeof num2);
+console.log(typeof  "10");
+
+//Ternary
+// text / true / false
+
+console.log(2 > 3 ? + "Sim" : "Não");
+
+let average = 5
+
+console.log(average >= 7 ? "Aprovado" : "Reprovado");
+
+
