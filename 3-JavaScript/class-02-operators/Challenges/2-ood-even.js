0b19445cc5ae1c4bd216bed041724f774bc3fafe
Matheus Barros Lima <matheusmatheusbarroslima6@gmail.com>
2023-12-07 16:21:20 -0300

    add challenges

---
+++ 3-JavaScript/class-02-operators/Challenges/2-ood-even.js
@@ -0,0 +1,8 @@
+const number = Number(prompt("Digite um número para saber se é par ou é ímpar: "));
+const result = numUser % 2 == 0 ? "Par" : number % 2 == 1 ? "Ímpar" : "Inválido";
+
+window.alert(`O número ${number} é ${result}`);
+
+
+
+
--- 3-JavaScript/class-02-operators/Challenges/5-ood-even.js
+++
@@ -1,8 +0,0 @@
-const numUser = Number(prompt("Digite um número para saber se é par ou é ímpar: "));
-const result = numUser % 2 == 0 ? "Par" : "Ímpar";
-
-window.alert(`O número ${numUser} é ${result}`);
-
-
-
-
