c2464973f77322a0ee05272c43fead7b30ad6110
Éric Batista <batistaeric62@gmail.com>
2024-02-06 16:19:55 -0300

    add class 3 and class 4 asychronism

--- 3-JavaScript/class-08-asynchronism/2-promises.js
+++ 3-JavaScript/class-08-asynchronism/2-promises.js
@@ -1,7 +1,12 @@
 const API = 
 "https://api.github.com/users/MatheusBarrosLima";
 
+console.log("Antes");
+
 const userGitHub = fetch(API)
- .then((res) => res.json())
- .then((data) => data );
-console.log(userGitHub);
+.then((res) => res.json())
+.then((data) => console.log(data))
+.catch((error) => console.error(error))
+.finally(()=> console.log("Fim da requisão!!!"));
+
+console.log("Depois");
