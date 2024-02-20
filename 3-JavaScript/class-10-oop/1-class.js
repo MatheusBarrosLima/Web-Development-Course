d82443deb686655af4d5a058be425c7f9b96613f
Matheus Barros Lima <matheusmatheusbarroslima6@gmail.com>
2024-02-20 16:22:26 -0300

    add class 1-class

---
+++ 3-JavaScript/class-10-oop/1-class.js
@@ -0,0 +1,66 @@
+//OOP - paradigma 
+// entitites or objects (real world)
+// properties (attibutes / variables)
+// methods (functions)
+
+class Control {
+    constructor(temp) {
+        this.temperature = temp;
+        this.color = "white";
+        this.isOn = false;
+    };
+    on_Off(){
+        this.isOn = !this.isOn;
+
+    }
+
+    tempUpper() {
+        this.temperature = this.temperature +1;
+    }
+
+    tempLower() {
+        this.temperature = this.temperature -1;
+    }
+}
+
+const airControl = new Control (18);
+const airControl2 = new Control (22);
+
+console.log(airControl.temperature);
+console.log(airControl2.temperature);
+console.log(airControl.color);
+
+
+
+
+console.log(airControl.isOn);
+
+airControl.tempUpper();
+console.log(airControl.temperature);
+
+airControl.tempLower();
+console.log(airControl.temperature);
+
+const buttonUpper =
+ document.querySelector(".buttonUpper");
+const buttonLower =
+document.querySelector(".buttonLower"); 
+const buttonOnOff =
+document.querySelector(".buttonOnOff");
+
+buttonUpper.addEventListener("click", () =>{
+    airControl.tempUpper();
+    console.log(airControl.temperature);
+});
+
+buttonLower.addEventListener("click", () =>{
+    airControl.tempLower();
+    console.log(airControl.temperature);
+});
+
+buttonOnOff.addEventListener("click", () =>{
+    airControl.on_Off();
+    console.log(airControl.isOn);
+})
+
+airControl.on_Off()
