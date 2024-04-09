720211bdfcbb310fb83896355754e78e1019ea37
Matheus Barros Lima <matheusmatheusbarroslima6@gmail.com>
2024-04-09 16:38:35 -0300

    add sql examples create and insert

---
+++ 5-SQL/class-01-type-commands.sql
@@ -0,0 +1,11 @@
+-- DDL
+CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    cpf TEXT NOT NULL UNIQUE,
+    age NUMERIC
+);
+
+-- DML
+INSERT INTO students (name, cpf, age) 
+VALUES ('Gohan', '123.123.123-01', 12)
