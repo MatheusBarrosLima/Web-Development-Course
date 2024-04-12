c8c095f5f5a054f3a621e3f99e105d9b369ad96a
Matheus Barros Lima <matheusmatheusbarroslima6@gmail.com>
2024-04-12 16:17:00 -0300

    add class 01 commands type

--- 5-SQL/class-01-type-commands.sql
+++ 5-SQL/class-01-type-commands.sql
@@ -1,4 +1,5 @@
 -- DDL
+DROP TABLE students;
 CREATE TABLE IF NOT EXISTS students (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
@@ -8,4 +9,32 @@ CREATE TABLE IF NOT EXISTS students (
 
 -- DML
 INSERT INTO students (name, cpf, age) 
-VALUES ('Gohan', '123.123.123-01', 12)
+VALUES 
+('Alucard', '123.123.123-04', 600),
+('Escanor', '123.123.123-05', 40),
+('Levi', '123.123.123-06', 30);
+
+--DQL
+SELECT * FROM students;
+
+SELECT name, age FROM students;
+
+SELECT * FROM students
+WHERE id >= 2 LIMIT 3;
+
+SELECT * FROM students
+WHERE age >= 18 AND age <=70;
+
+--TCL
+BEGIN TRANSACTION;
+ROLLBACK;
+COMMIT;
+
+--DDL
+ALTER TABLE students
+ADD COLUMN anime TEXT;
+
+UPDATE students
+SET anime= 'Shingeki no Kyojin'
+WHERE id = 3;
+
