b1700d9759eb8c36f4b26fcd2376880091bdd768
Matheus Barros Lima <matheusmatheusbarroslima6@gmail.com>
2024-04-18 16:33:30 -0300

    uptade class 02 inner join

--- 5-SQL/class-02-data-modeling.sql
+++ 5-SQL/class-02-data-modeling.sql
@@ -13,12 +13,18 @@ CREATE TABLE IF NOT EXISTS courses(
     workload VARCHAR CHECK (workload IN ('40h', '60h', '160h', '240h')) DEFAULT '160h'
 );
 
+SELECT * FROM students; 
 INSERT INTO students (name, id_course) VALUES
 ('Neji', 1),
 ('Shikamaru', 1),
 ('Rock lee', 2);
 
+SELECT * FROM courses;
 INSERT INTO courses (name, workload) VALUES
 ('Controle do Chakra', '40h'),
 ('Jutsu clone das sombras', '160h'),
 ('Jutsu de Invocação', '240h');
+
+SELECT students.name, courses.name AS course
+FROM students INNER JOIN courses
+ON students.id_course = courses.id;     
