560904a17dd8507525f0803010772ae417b158d2
Matheus Barros Lima <matheusmatheusbarroslima6@gmail.com>
2024-04-29 16:39:09 -0300

    add class 01 server

---
+++ 6-Node.js/class-01-server/src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import "dotenv/config"
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}...`);
+});
+
+app.get("/", (_request, response) => {
+  response.json({ className: "Class 01 Server" });
+});
